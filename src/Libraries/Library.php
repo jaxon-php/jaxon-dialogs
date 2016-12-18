597e1c30b3e635728acd462da3d1b1949171fa18
feuzeu <thierry.feuzeu@gmail.com>
2016-12-18 07:27:12 -0500

    Added libraries management classes.

---
+++ src/Libraries/Library.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Jaxon\Dialogs\Libraries\Bootbox;
+
+use Jaxon\Dialogs\Interfaces\Plugin;
+
+class Library implements Plugin
+{
+    protected $dialog = null;
+    protected $name = null;
+
+    public function setDialog($dialog)
+    {
+        $this->dialog = $dialog;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getJs()
+    {
+        return '';
+    }
+
+    public function getCss()
+    {
+        return '';
+    }
+
+    public function getScript()
+    {
+        return '';
+    }
+
+    /**
+     * Get the value of a config option
+     *
+     * @param string        $sName            The option name
+     * @param mixed         $xDefault         The default value, to be returned if the option is not defined
+     *
+     * @return mixed        The option value, or its default value
+     */
+    public function getOption($sName, $xDefault = null)
+    {
+        $sName = 'dialogs.' . $this->getName() . '.' . $sName;
+        return $this->dialog->getOption($sName, $xDefault);
+    }
+    
+    /**
+     * Check the presence of a config option
+     *
+     * @param string        $sName            The option name
+     *
+     * @return bool        True if the option exists, and false if not
+     */
+    public function hasOption($sName)
+    {
+        $sName = 'dialogs.' . $this->getName() . '.' . $sName;
+        return $this->dialog->hasOption($sName);
+    }
+    
+    /**
+     * Get the names of the options matching a given prefix
+     *
+     * @param string        $sPrefix        The prefix to match
+     *
+     * @return array        The options matching the prefix
+     */
+    public function getOptionNames($sPrefix)
+    {
+        $sPrefix = 'dialogs.' . $this->getName() . '.' . $sPrefix;
+        $this->dialog->getOptionNames($sPrefix);
+    }
+}
